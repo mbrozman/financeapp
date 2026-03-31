a52843b2f9fbdd37ed331819637a5aa49068bd05
mbrozman <mi.brozman@gmail.com>
2026-03-31 11:23:18 +0200

    oprava importu dat

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,18 @@ Route::get('/admin/sync-portfolio-data', function () {
     \Illuminate\Support\Facades\Artisan::call('app:init-benchmarks');
     echo \Illuminate\Support\Facades\Artisan::output() . "<br>";
 
+    echo "2. AKTUALIZUJEM CENY VŠETKÝCH AKCIÍ...<br>";
+    \Illuminate\Support\Facades\Artisan::call('app:update-stock-prices');
+    echo \Illuminate\Support\Facades\Artisan::output() . "<br>";
+
     // Spätná oprava is_benchmark príznaku
     \App\Models\Investment::whereIn('ticker', ['SPY', 'QQQ'])->update(['is_benchmark' => true]);
 
-    echo "2. Vytváram snímku portfólia pre všetkých používateľov...<br>";
+    echo "3. Vytváram snímku portfólia pre všetkých používateľov...<br>";
     \Illuminate\Support\Facades\Artisan::call('app:take-portfolio-snapshot');
     echo \Illuminate\Support\Facades\Artisan::output() . "<br>";
 
-    echo "3. Generujem históriu (7 dní) pre všetkých používateľov...<br>";
+    echo "4. Generujem históriu (7 dní) pre všetkých používateľov...<br>";
     \App\Models\User::all()->each(function ($user) {
         $latest = \App\Models\PortfolioSnapshot::where('user_id', $user->id)->orderBy('recorded_at', 'desc')->first();
         if ($latest) {
